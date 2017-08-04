93e024e21bbf2b6c6c0362fa19a7c5038059f1e8
JHTA <JHTA@JHTA-PC>
2017-08-04 12:37:34 +0900

    sd

--- semi_team1/src/com/team1/test/jsoupTest.java
+++ semi_team1/src/com/team1/test/jsoupTest.java
@@ -3,27 +3,29 @@ package com.team1.test;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.crypto.CipherInputStream;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class jsoupTest {
 	public static void main(String[] args) {
 		try {
 
-			Document doc = Jsoup.connect("http://movie.naver.com/movie/sdb/rank/rmovie.nhn").get();
-			Elements title = doc.select(".title");
-
-			ArrayList<String> list1 = new ArrayList<>();
-
-			for (Element e : title) {
-				System.out.println(e.text());
-				list1.add(e.text());
-			}
-
-			Elements rank = doc.select(".r_ranking li a");
-			System.out.println(rank.text());
+			/*
+			 * Document doc =
+			 * Jsoup.connect("http://movie.naver.com/movie/sdb/rank/rmovie.nhn")
+			 * .get(); Elements title = doc.select(".title");
+			 * 
+			 * ArrayList<String> list1 = new ArrayList<>();
+			 * 
+			 * for (Element e : title) { System.out.println(e.text());
+			 * list1.add(e.text()); }
+			 * 
+			 * Elements rank = doc.select(".r_ranking li a");
+			 * System.out.println(rank.text());
+			 */
 
 			/////////////////////////////////////////////////////////////////////////////////////////////////////////
 			Document doc1 = Jsoup.connect("http://score.sports.media.daum.net/record/soccer/epl/trnk.daum").get();
@@ -33,16 +35,21 @@ public class jsoupTest {
 			ArrayList<String> list[] = new ArrayList[20];
 			int z = 0;
 			for (int i = 0; i < 20; i++) {
-
 				list[i] = new ArrayList<>();
 				for (int j = 0; j < 11; j++) {
+					if (str[z].contains("W") || str[z].contains("C")) {
+						z++;
+					}
 					list[i].add(str[z]);
 					z++;
 				}
 			}
 
-			for (String str1 : list[0]) {
-				System.out.println(str1);
+			list[9].set(1, "W.브로미치");
+			list[13].set(1, "W.팰리스");
+
+			for (String s : list[9]) {
+				System.out.println(s);
 			}
 
 		} catch (IOException e) {
